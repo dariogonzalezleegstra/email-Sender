3aedd1d6bc334154ba00877f73917b3e964aa0e4
dariogonzalezleegstra <daroo.lp@gmail.com>
2017-09-18 23:13:29 -0300

    surveyRoutes map added

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -9,12 +9,18 @@ module.exports = app => {
     app.post('/surveys', requireLogin, requireCredits, (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
-        //The same as title: title, subject: subject...
         const survey = new Survey ({
             title,
             subject,
             body,
-            recipient
+            recipient: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: req.user.id, //id is available in every mongoose model
+            dateSent: Date.now()
         });
     });
 };
+
+
+//title, subject, etc is the same as title: title, subject: subject...
+//return { email: email } email.trim() es para sacar los espacios.
+//recipient es el array de mails separado por comas. lo descompongo en objetos
